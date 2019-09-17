0512bcec1db8323a5e94c9806b69c350679e0c94
Uros Randelovic <uros.randelovic@gmail.com>
2019-09-17 19:19:14 -0400

    messing with db

--- Db.js
+++ Db.js
@@ -1,19 +1,37 @@
 export function uploadIssue(issueType, reporterData, imgUrl, zip, status, coords) {
+    body = {
+        issueType: issueType,
+        reporterData: reporterData,
+        imgUrl: imgUrl,
+        zip: zip,
+        status: status,
+        coords: coords
+    }
+    
+    const createFormData = (imageUrl, body) => {
+        const data = new FormData();
+      
+        data.append("imageUrl", {
+          name: "not important",
+          type: jpeg,
+          uri: imgUrl
+        });
+      
+        Object.keys(body).forEach(key => {
+          data.append(key, body[key]);
+        });
+      
+        return data;
+    };
 
+    console.log()
     fetch('https://desolate-lowlands-52819.herokuapp.com/addIssue', {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
             },
-            body: {
-                issueType: issueType,
-                reporterData: reporterData,
-                imgUrl: imgUrl,
-                zip: zip,
-                status: status,
-                coords: coords
-            },
+            body: JSON.stringify(createFormData),
         }).then((response) => response.json())
         .then((responseJson) => {
             console.log(responseJson)
